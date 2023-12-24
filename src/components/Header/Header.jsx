446efeae2d42504d8d4fc535f8cdb79ffa413150
Ndukwe Chima <chimandukwe.r@gmail.com>
2023-12-24 21:22:20 +0100

    worked on the routing to correct some issues

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ import Logo from "../../assets/images/Logo.png";
 const GetStartedButton = ({ to, children }) => (
   <NavLink
     to={to}
-    className="!bg-pink900 text-white px-4 font-bold py-2 rounded"
+    className="!bg-pink900 hover:opacity-80 text-white px-4 font-bold py-2 rounded"
   >
     {children}
   </NavLink>
@@ -72,31 +72,31 @@ const Header = () => {
         <div className="md:hidden absolute top-16 right-1 flex flex-col bg-skyBlue900 text-white p-4 rounded">
           <NavLink
             to="/"
-            className="hover:text-skyBlue900 px-3 py-2 transition duration-300"
+            className="hover:opacity-40 px-3 py-2 transition duration-300"
           >
             Home
           </NavLink>
           <NavLink
             to="/services"
-            className="hover:text-skyBlue900 px-3 py-2 transition duration-300"
+            className="hover:opacity-40 px-3 py-2 transition duration-300"
           >
             Services
           </NavLink>
           <NavLink
             to="/about"
-            className="hover:text-skyBlue900 px-3 py-2 transition duration-300"
+            className="hover:opacity-40 px-3 py-2 transition duration-300"
           >
             About Us
           </NavLink>
           <NavLink
             to="/contact"
-            className="hover:text-skyBlue900 px-3 py-2 transition duration-300"
+            className="hover:opacity-40 px-3 py-2 transition duration-300"
           >
             Contact Us
           </NavLink>
           <NavLink
-            to="/search"
-            className="hover:text-skyBlue900 px-3 py-2 transition duration-300"
+            to="/searchcustomer"
+            className="hover:opacity-40 px-3 py-2 transition duration-300"
           >
             Search
           </NavLink>
